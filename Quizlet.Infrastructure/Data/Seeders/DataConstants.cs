a89ef9193fbabbac6f9a015ba6209177747a3ca9
Kristian Dimitrov <krisdimitrov03@students.softuni.bg>
2023-02-02 01:00:47 +0200

    Seeder Structure Added

---
+++ Quizlet.Infrastructure/Data/Seeders/DataConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizlet.Infrastructure.Data.Seeders
+{
+    public static class DataConstants
+    {
+        public const string Path = "../Quizlet.Infrastructure/Seeders/Data/{0}";
+
+        public const string Users = "users.json";
+    }
+}
--- Quizlet.Infrastructure/Data/Seeders/Seeder.cs
+++ Quizlet.Infrastructure/Data/Seeders/Seeder.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Quizlet.Infrastructure.Data.Models.Auth;
 
 namespace Quizlet.Infrastructure.Data.Seeders
 {
@@ -11,7 +9,31 @@ namespace Quizlet.Infrastructure.Data.Seeders
     {
         public static void Seed(IApplicationBuilder builder)
         {
+            using (var serviceScope = builder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<QuizletContext>();
 
+                context.Database.EnsureCreated();
+
+                AddData<ApplicationUser>(context, DataConstants.Users);
+            }
+        }
+
+        private static void AddData<T>(QuizletContext context, string fileName)
+            where T : class
+        {
+            if (!context.Set<T>().Any())
+            {
+                var data = new List<T>();
+
+                using (var reader = new StreamReader(string.Format(DataConstants.Path, fileName)))
+                {
+                    data = JsonConvert.DeserializeObject<List<T>>(reader.ReadToEnd());
+                }
+
+                context.Set<T>().AddRange(data);
+                context.SaveChanges();
+            }
         }
     }
 }
